c870442368239fbbdcb4a03b035c6f1aa9307889
Liam <liamnguyen224@gmail.com>
2024-10-09 18:32:46 -0700

    connect 2nd test

--- main.js
+++ main.js
@@ -10,18 +10,6 @@ navToggle.addEventListener("click", () => {
   primaryHeader.toggleAttribute("data-overlay");
 });
 
-const slider = new A11YSlider(document.querySelector(".slider"), {
-  adaptiveHeight: false,
-  dots: true,
-  centerMode: true,
-  arrows: false,
-  responsive: {
-    480: {
-      dots: false, // dots enabled 1280px and up
-    },
-  },
-});
-
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
   anchor.addEventListener("click", function (e) {
     e.preventDefault();
@@ -32,53 +20,11 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
   });
 });
 
-// slider functions
-let slide = document.querySelector(".slide .listMeat");
-let items = document.querySelectorAll(".slide .listMeat .itemMeat");
-let next = document.getElementById("next");
-let prev = document.getElementById("prev");
-let dots = document.querySelectorAll(".slide .dotsMeat li");
-
-let lengthItems = items.length - 1;
-let active = 0;
-next.onclick = function () {
-  active = active + 1 <= lengthItems ? active + 1 : 0;
-  reloadSlider();
-};
-prev.onclick = function () {
-  active = active - 1 >= 0 ? active - 1 : lengthItems;
-  reloadSlider();
-};
-let refreshInterval = setInterval(() => {
-  next.click();
-}, 3000);
-function reloadSlider() {
-  slide.style.left = -items[active].offsetLeft + "px";
-
-  let last_active_dot = document.querySelector(".slide .dotsMeat li.active");
-  last_active_dot.classList.remove("active");
-  dots[active].classList.add("active");
-
-  clearInterval(refreshInterval);
-  refreshInterval = setInterval(() => {
-    next.click();
-  }, 3000);
-}
-
-dots.forEach((li, key) => {
-  li.addEventListener("click", () => {
-    active = key;
-    reloadSlider();
-  });
-});
-window.onresize = function (event) {
-  reloadSlider();
-};
-
 // form handlers
 // Find the form and add an event listener for submit
 document.querySelector("form").addEventListener("submit", function (e) {
   e.preventDefault();
+  console.log("Form submission prevented, handling via JS"); // Test it in the browser console
 
   const firstName = document.querySelector('input[name="first_name"]').value;
   const lastName = document.querySelector('input[name="last_name"]').value;
